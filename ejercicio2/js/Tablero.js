e14e250bf8382413d90df1a5ed39a40d3ce79a33
Raúl-Labrador <raullabrador10@gmail.com>
2025-11-06 20:32:07 +0100

    Borrado de malla

--- ejercicio2/js/Tablero.js
+++ ejercicio2/js/Tablero.js
@@ -12,17 +12,6 @@ class Tablero {
 
     init(config) {
         this.size = config.size;
-        
-        let malla = Array.from({ length: this.size }, (v, i) => 
-            Array.from({ length: this.size }, (v, j) => new Celda( i,j))
-        );
-        malla.forEach((item,i)=>item.forEach((celda,j)=>{
-            if(i>0) celda.nuevoVecino(Vecinos.ARRIBA,malla[i-1][j]);
-            if(i<this.size-1) celda.nuevoVecino(Vecinos.ABAJO,malla[i+1][j]);
-            if(j>0) celda.nuevoVecino(Vecinos.IZQUIERDA,malla[i][j-1]);
-            if(j<this.size-1) celda.nuevoVecino(Vecinos.DERECHA,malla[i][j+1]);
-        }));
-        this.tablero=malla.flat();
         this.creaGridTemplate(this.size, config.casilla, config.table);
         this.submarino.init(this.size);
     }
