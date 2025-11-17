d0eb79ee9546a3fe3ddf29c10cdedbcfb1972972
Raúl-Labrador <raullabrador10@gmail.com>
2025-11-17 14:01:01 +0100

    clase 17-11

--- simonDice/js/UI.js
+++ simonDice/js/UI.js
@@ -4,12 +4,48 @@ export const UI = {
 		green: null,
 		blue: null,
 		yellow: null,
+		secuencia: [],
+		colores: [],
 	},
 
-    init: (domControl) => {
-        UI.control.red = document.getElementById(domControl.red);
-        UI.control.green = document.getElementById(domControl.green);
-        UI.control.blue = document.getElementById(domControl.blue);
-        UI.control.yellow = document.getElementById(domControl.yellow);
-    },
+	init: (domControl) => {
+		UI.control.red = document.getElementById(domControl.red);
+		UI.control.green = document.getElementById(domControl.green);
+		UI.control.blue = document.getElementById(domControl.blue);
+		UI.control.yellow = document.getElementById(domControl.yellow);
+		UI.control.colores = [
+			UI.control.red,
+			UI.control.blue,
+			UI.control.yellow,
+			UI.control.green,
+		];
+	},
+	pulsarTecla: (conf) => {
+		conf.tecla.style.opacity = 1;
+		setTimeout(() => {
+			conf.tecla.style.opacity = 0.5;
+			setTimeout(() => {
+				usarTecla.start();
+			}, 2000);
+		}, 2000);
+	},
+	aleatorio: () => {
+		let numeroRandom = Math.floor(Math.random() * UI.control.colores.length);
+		console.log(numeroRandom);
+
+		let colorSeleccionado = UI.control.colores[numeroRandom];
+
+		usarTecla.addTecla(document.getElementById(colorSeleccionado));
+	},
+	usarTecla: {
+		lista: [],
+		addTecla: (tecla) => {
+			usarTecla.lista.push({ tecla: tecla });
+			secuencia.push({ tecla: tecla });
+		},
+		start: () => {
+			let teclaPulsada = usarTecla.lista.shift();
+			if (teclaPulsada != undefined) pulsarTecla(teclaPulsada);
+		},
+	},
 };
--- simonDice/js/script.js
+++ simonDice/js/script.js
@@ -5,4 +5,106 @@ UI.init({
     green: 'green',
     blue: 'blue',
     yellow: 'yellow'
-})
+});
+
+// UI.usarTecla.start();
+
+
+// const pulsarTecla = (conf) => {
+//     conf.tecla.style.backgroundColor = conf.colorOn;
+//     setTimeout(() => {
+//         conf.tecla.style.backgroundColor = conf.colorOff;
+//         setTimeout(()=> {
+//             usarTecla.start();
+//         },2000)
+//     },2000)
+// }
+
+// const usarTecla = {
+//     lista: [],
+//     addTecla: (tecla, colorOn, colorOff) => {
+//         usarTecla.lista.push({tecla:tecla, colorOn:colorOn, colorOff:colorOff});
+//     },
+//     start: () => {
+//         let teclaPulsada = usarTecla.lista.pop();
+//         console.log(teclaPulsada);
+//         pulsarTecla(teclaPulsada);
+//     }
+// }
+
+// usarTecla.addTecla(document.getElementById("red"), "red", "blue");
+// usarTecla.addTecla(document.getElementById("red"), "yellow", "green");
+
+// usarTecla.start();
+
+
+// Mi forma (con opacidad)
+
+const colores = ["red", "blue", "yellow", "green"];
+
+const secuencia = [];
+
+const rojo = document.getElementById(colores[0]);
+const azul = document.getElementById(colores[1]);
+const amarillo = document.getElementById(colores[2]);
+const verde = document.getElementById(colores[3]);
+
+const setEvent = () => {
+
+    rojo.addEventListener('click', (event) => {
+        console.log("rojo")
+    });
+
+    azul.addEventListener('click', (event) => {
+        console.log("azul")
+    });
+
+    amarillo.addEventListener('click', (event) => {
+        console.log("amarillo")
+    });
+
+    verde.addEventListener('click', (event) => {
+        console.log("verde")
+    });
+}
+
+// Ponemos para que los colores se puedan pulsar
+const pulsarTecla = (conf) => {
+    conf.tecla.style.opacity = 1;
+    setTimeout(() => {
+        conf.tecla.style.opacity = 0.5;
+        setTimeout(()=> {
+            usarTecla.start();
+        },2000)
+    },2000)
+}
+
+const usarTecla = {
+    lista: [],
+    addTecla: (tecla) => {
+        usarTecla.lista.push({tecla:tecla});
+        secuencia.push({tecla:tecla});
+    },
+    start: () => {
+        let teclaPulsada = usarTecla.lista.shift();
+        if(teclaPulsada != undefined) pulsarTecla(teclaPulsada);
+    }
+}
+
+// Creamos una función para que nos devuelva un numero aletarorio, y que esto nos devuelva uno de los 4 colores, para que se enciendan y apaguen de manera random 
+const aleatorio = () => {    
+    let numeroRandom = Math.floor(Math.random() * colores.length);
+    console.log(numeroRandom)
+
+    let colorSeleccionado = colores[numeroRandom];
+
+    usarTecla.addTecla(document.getElementById(colorSeleccionado));
+}
+
+aleatorio();
+aleatorio();
+aleatorio();
+aleatorio();
+
+usarTecla.start();
+setEvent();
