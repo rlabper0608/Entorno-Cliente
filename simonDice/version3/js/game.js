08ec430495e7d39b3c37d38ad465400c347c6f4e
Raúl-Labrador <raullabrador10@gmail.com>
2025-12-02 16:20:22 +0100

    Creación de la versión 3 para meterle la API y las animaciones

---
+++ simonDice/version3/js/game.js
@@ -0,0 +1,102 @@
+export class game {
+	listButtons = [];
+	list = [];
+	secuenciaUsuario = [];
+	constructor(UIControl) {
+		this.UIControl = UIControl;
+		this.UIControl.start(this);
+		this.listButtons = this.UIControl.listButtons;
+		this.setEvent();
+	}
+
+	aleatorio() {
+		console.log("aleatorio");
+		let numeroRandom = Math.floor(Math.random() * this.listButtons.length);
+		// console.log(numeroRandom);
+
+		this.list.push(numeroRandom);
+		// console.log(this.list);
+	}
+
+	change(element, status) {
+		return new Promise((resolve) => {
+			// console.log(element);
+			setTimeout(() => {
+				element.color.style.backgroundColor =
+					status === this.UIControl.status.ON
+						? element.colorOn
+						: element.colorOff;
+				resolve(true);
+			}, 2000);
+		});
+	}
+
+	async play() {
+		console.log("play");
+		this.aleatorio();
+		this.UIControl.busy = true;
+		// this.removeEvent();
+
+		for (let item of this.list) {
+			await this.change(this.listButtons[item], this.UIControl.status.ON);
+			await this.change(this.listButtons[item], this.UIControl.status.OFF);
+		}
+
+		console.log("termine");
+		this.UIControl.busy = false;
+		// this.setEvent();
+	}
+
+	comprobar(secuencia, lista) {
+		for (let i = 0; i < secuencia.length; i++) {
+			if (secuencia[i] !== lista[i]) {
+				console.log("fallaste");
+				this.reiniciar();
+				return false;
+			}
+		}
+
+		if (lista.length === secuencia.length) {
+			console.log("Secuencia Correcta");
+			this.secuenciaUsuario = [];
+			this.play();
+		} else {
+			console.log("Vas bien");
+		}
+	}
+
+	setEvent() {
+		if (!this.UIControl.busy) {
+			this.listButtons.forEach((item, index) => {
+				// console.log(item);
+				// console.log(index);
+				item.color.addEventListener("click", () => {
+					this.secuenciaUsuario.push(index);
+					console.log(this.secuenciaUsuario);
+
+					this.comprobar(this.secuenciaUsuario, this.list);
+				});
+			});
+		}
+	}
+
+	removeEvent() {
+		if (this.UIControl.busy) {
+			this.listButtons.forEach((item, index) => {
+				// console.log(item);
+				// console.log(index);
+				item.color.removeEventListener("click", () => {
+					this.secuenciaUsuario.push(index);
+				});
+			});
+		}
+	}
+
+	reiniciar() {
+		console.log("reiniciar");
+		this.list = [];
+		this.secuenciaUsuario = [];
+		this.UIControl.busy = false;
+		this.UIControl.playButton.hidden = false;
+	}
+}
